8f819b16edacb7461db58f8bbedcafb8866a5d2a
Gokul4reaL <gravekrishna@gmail.com>
2024-11-15 15:29:10 +0530

    Hardcoded Changes

--- cartservice/src/main/java/com/cartservice/controller/CartRestController.java
+++ cartservice/src/main/java/com/cartservice/controller/CartRestController.java
@@ -13,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cartservice.model.CartItem;
+import com.cartservice.model.Session;
 import com.cartservice.model.User;
+import com.cartservice.repository.SessionRepository;
 import com.cartservice.repository.UserRepository;
 import com.cartservice.service.CartService;
 import com.cartservice.service.ProductService;
@@ -29,17 +31,21 @@ public class CartRestController {
 
     @Autowired
     private ProductService productService;
+    
+    @Autowired
+    private SessionRepository sessionRepo;
 
     @Autowired
     private UserRepository userRepository;
 	@GetMapping("/getCartItems")
     public ResponseEntity<List<CartItem>> showCart(HttpSession session, Model model) {
-       // String email = (String) session.getAttribute("email");
-//        if (email == null) {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//        }
+       
+		
+		Optional<Session> ses = sessionRepo.findById(1L);
+		int value = ses.get().getValue();
+		String email = ses.get().getEmail();
         
-        Optional<User> user = userRepository.findByEmail("gravekrishna@gmail.com");
+        Optional<User> user = userRepository.findByEmail(email);
         if (user.isPresent()) {
             List<CartItem> cartItems = cartService.getCartItemsByUser(user.get());
             
